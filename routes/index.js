eb7ff3db1556e566c19623b8c656621f5305c305
xbx <1060031655@qq.com>
2016-12-01 13:37:30 +0800

    start

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var data = {
+		title: 'Express',
+		message: "welcome to FeidianStudio"
+	}
+  	res.render('index', data );
 });
 
 module.exports = router;
