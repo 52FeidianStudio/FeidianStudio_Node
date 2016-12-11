5a8b3e19a62c010629e8654f13ddb796f83f629b
xbx <1060031655@qq.com>
2016-12-11 19:52:56 +0800

    welcome & index

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var welcome = require('./routes/welcome');
+var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -22,7 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', welcome);
+app.use('/welcome', welcome);
+app.use('/index', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
